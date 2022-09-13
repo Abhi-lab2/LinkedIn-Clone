c4e93f8b7674bb386ea2cd24946e51c130a0ed96
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-09-13 18:04:35 +0530

    widget and feed add and modified

---
+++ src/Feed.js
@@ -0,0 +1,102 @@
+import ArticleIcon from "@mui/icons-material/Article";
+import CreateIcon from "@mui/icons-material/Create";
+import EventIcon from "@mui/icons-material/Event";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import { Avatar } from "@mui/material";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import FlipMove from "react-flip-move";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/counter/userSlice";
+import "./Feed.css";
+import { db } from "./firebase";
+import InputOption from "./InputOption";
+import Modal from "./Modal";
+import Post from "./Post";
+import PostModal from "./PostModal";
+
+function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    // firebase-version 9 (latest)
+    onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+  }, []);
+
+  const [showModal, setShowModal] = useState(true);
+  const openModal = () => {
+    setShowModal(!showModal);
+  };
+  const user = useSelector(selectUser);
+  const [showModal_2, setShowModal_2] = useState("close");
+
+  const handleClick = () => {
+    // e.preventDefault();
+    // if (e.target !== e.currentTarget) {
+    //   return;
+    // }
+
+    switch (showModal_2) {
+      case "open":
+        setShowModal_2("close");
+        break;
+      case "close":
+        setShowModal_2("open");
+        break;
+      default:
+        setShowModal_2("close");
+        break;
+    }
+  };
+  return (
+    <div className="feed">
+      <div className="feed_inputContainer">
+        <div>
+          <Avatar sx={{ width: 50, height: 50 }} src={user.photoURL}>
+            {user.email[0].toUpperCase()}
+          </Avatar>
+          <div className="feed_input">
+            <button onClick={handleClick} className="search_btn">
+              <CreateIcon />
+              Start a post
+            </button>
+          </div>
+        </div>
+
+        <div className="feed_inputOptions">
+          <InputOption
+            OpenModal={openModal}
+            Icon={ImageIcon}
+            title="Photo"
+            color="#70B5F9"
+          />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#7FC15E" />
+          <InputOption Icon={EventIcon} title="Event" color="#E7A33E" />
+          <InputOption Icon={ArticleIcon} title="Article" color="#FC9295" />
+        </div>
+      </div>
+      {/* Modal here */}
+
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title_option="videooo"
+      />
+      <PostModal showModal={showModal_2} handleClick={handleClick} />
+      <div className="posts">
+        <FlipMove>
+          {posts.map((post) => (
+            <Post key={post.id} id={post.id} post={post.data()} />
+          ))}
+        </FlipMove>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/Widget.js
@@ -0,0 +1,55 @@
+import AddIcon from "@mui/icons-material/Add";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import InfoIcon from "@mui/icons-material/Info";
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Widgets.css";
+import WidgetsButton from "./WidgetsButton";
+
+function Widget() {
+  const singleWidgets = (title, details) => (
+    <div className="single_widgets">
+      <Avatar
+        sx={{
+          width: 50,
+          height: 50,
+          backgroundColor: "white",
+          color: "#0a66c2",
+          border: "1px solid #0a66c2",
+        }}
+        src={""}
+      >
+        {title[0].toUpperCase()}
+      </Avatar>
+      <div className="single_widgets_description">
+        <h2>{title}</h2>
+        <p>
+          Company{" . "}
+          {details}
+        </p>
+        <WidgetsButton Icon={AddIcon} title="Follow" />
+      </div>
+    </div>
+  );
+  return (
+    <div className="widgets">
+      <div className="widgets_header">
+        <h2>Add to you feed</h2>
+        <InfoIcon />
+      </div>
+      <div className="widgets_body">
+        {singleWidgets("Web Developer", "Program Development")}
+        {singleWidgets("Geeks for Geeks", "Computer Software")}
+        {singleWidgets("React Jobs", "Staffings and Recruiting")}
+      </div>
+      <div className="widgets_footer">
+        <button>
+          View all recommendations
+          <ArrowForwardIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Widget;
